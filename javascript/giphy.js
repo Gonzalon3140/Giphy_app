ef249135fe8ab5643ae9fb2272e2903a61c40d85
Gonzalon <40928276+Gonzalon3140@users.noreply.github.com>
2018-09-29 22:36:18 -0700

    still have problems with the buttons not reseting and same with the images

--- javascript/giphy.js
+++ javascript/giphy.js
@@ -4,42 +4,79 @@ $(document).ready(function () {
 
     let apiKey = "X4PhEVbTgYb63FySEkmIIqnIG4C6m1z9";
 
-
-
     function getGifs(movie) {
-        let queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=" + apiKey + "&limit=5";
+        let queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=" + apiKey + "&limit=10";
         $.ajax({
             url: queryUrl,
             method: "GET"
         }).then(function (response) {
-            console.log(response)
             for (let i = 0; i < response.data.length; i++) {
-                let giphy = $('<div>').append(gifWrapper)
-                let rating = $('<p>').append()
-                let title = $('<h3>').append()
-                let image = $("<img>").attr("src", image);
-                giphy.addClass("giphy").text(response.data[i].title);
+                console.log(i)
+                console.log(response.data[i])
+                let giphy = $('<div>');
+                let rating = $('<p>').text(response.data[i].rating);
+                let title = $('<h3>').text(response.data[i].title);
+                let image = $("<img>").attr("src", response.data[i].images.fixed_height_still.url);
+                image.attr("dataImage", response.data[i].images.fixed_height_still.url);
+                image.attr("dataAnimate", response.data[i].images.fixed_height.url);
+                image.attr("dataCurrent", "static");
+                image.addClass('giphyImage');
+                giphy.append(rating, title, image);
+                giphy.addClass("giphy");
+                rating.addClass('rating');
+                title.addClass('title');
                 $('#gifWrapper').append(giphy);
-            }
 
+            }
+            console.log(response.data)
+            for (let i = 0; i < response.data.length; i++) {
+                console.log("hit")
+
+            }
         })
     }
 
     function renderedButton() {
 
         for (let i = 0; i < movies.length; i++) {
-            let button = $('<button>')
+            let button = $('<button>');
             button.addClass("movieButton").text(movies[i]);
             $('#buttonWrapper').append(button);
+
         }
 
 
     }
+
+    $(document).on("click", ".giphyImage", function () {
+        if ($(this).attr("dataCurrent") === "static") {
+            $(this).attr("src", $(this).attr('dataAnimate'))
+            $(this).attr('dataCurrent', "animate");
+        } else {
+            $(this).attr("src", $(this).attr('dataImage'))
+            $(this).attr('dataCurrent', "static");
+
+        }
+    })
+
+    $(document).on("click", "#buttonView", function () {
+        var a = $("<button>");
+        a.addClass("movie-btn");
+        a.attr("data-name");
+        var ourtextinput = $('#exampleInputPassword1').val()
+        a.text(ourtextinput); //buttonview.text(movies[i])
+        $("#buttonWrapper").append(a);
+        console.log(this)
+        getGifs(ourtextinput);
+
+
+    })
+
     $(document).on('click', '.movieButton', function () {
         let movieName = $(this).text();
-        getGifs(movieName)
-        console.log(movieName)
-    })
+        getGifs(movieName);
+
+    });
     renderedButton();
 })
 
